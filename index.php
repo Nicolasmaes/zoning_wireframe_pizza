21012f6aa936c6d040c0710d22e5593e7a88a978
Nicolas <n.maes@codeur-online>
2021-03-25 16:24:22 +0100

    integration faite jusqu'à l'introduction

--- index.php
+++ index.php
@@ -20,11 +20,13 @@
             <h1>Italian<br>Pizza</h1>
         </div> <!-- fermeture mainpizza -->
     </div> <!-- fermture home -->
+    <div class="menu0">
     <h2 class="menu-title stf">menu</h2>
     <div class="menu">
+        <div class="cards"></div>
         <div class="card" style="width: 18rem;">
             <div class="card-body">
-                <img src="img/pizza-01.png" class="card-img-top" alt="pizza1">
+                <img src="img/pizza-01.png" class="card-img-top flip" alt="pizza1">
                 <h5 class="card-title pub">Basil Margherita</h5>
                 <p class="price pub"><span class="dollar">$</span>14.50</p>
                 <div class="taille">
@@ -34,12 +36,12 @@
                 </div> <!-- fermeture select-->
                 </div> <!-- fermeture taille-->
                 <p class="card-text pub">Margherita pizza starts fresh basil, mozzarella, and the bas tomatoes you can find.  It's still a delicious, simple showcase of good quality produce, but the addition of crisp, chewy pizza crust, melty cheese, and warm sauce makes it a cozier, heartier meal.</p>
-                <a href="#" class="btn btn-primary pub">Add to basket</a>
+                <a href="#" class="btn">Add to basket</a>
             </div> <!-- fermeture card-body -->
         </div> <!-- fermeture card -->
         <div class="card" style="width: 18rem;">
             <div class="card-body">
-                <img src="img/pizza-02.png" class="card-img-top" alt="pizza2">
+                <img src="img/pizza-02.png" class="card-img-top flip" alt="pizza2">
                 <h5 class="card-title pub">Jalapeño Popper</h5>
                 <p class="price pub"><span class="dollar">$</span>11.80</p>
                 <div class="taille">
@@ -49,12 +51,12 @@
                 </div> <!-- fermeture select-->
                 </div> <!-- fermeture taille-->
                 <p class="card-text pub">All the flavors of a cheesy jalapeño popper is so much better in pizza form, with added bacon of course ! Imagine a jalapeño popper in the form of a pizza. Yes, it's as epic as it sounds. And better than a traidtional jalapeño popper in my opinion.</p>
-                <a href="#" class="btn btn-primary pub">Add to basket</a>
+                <a href="#" class="btn">Add to basket</a>
             </div> <!-- fermeture card-body -->
         </div> <!-- fermeture card -->
         <div class="card" style="width: 18rem;">
             <div class="card-body">
-                <img src="img/pizza-01.png" class="card-img-top" alt="pizza1">
+                <img src="img/pizza-01.png" class="card-img-top flip" alt="pizza1">
                 <h5 class="card-title pub">Basil Margherita</h5>
                 <p class="price pub"><span class="dollar">$</span>14.50</p>
                 <div class="taille">
@@ -64,12 +66,12 @@
                 </div> <!-- fermeture select-->
                 </div> <!-- fermeture taille-->
                 <p class="card-text pub">Margherita pizza starts fresh basil, mozzarella, and the bas tomatoes you can find.  It's still a delicious, simple showcase of good quality produce, but the addition of crisp, chewy pizza crust, melty cheese, and warm sauce makes it a cozier, heartier meal.</p>
-                <a href="#" class="btn btn-primary">Add to basket</a>
+                <a href="#" class="btn">Add to basket</a>
             </div> <!-- fermeture card-body -->
         </div> <!-- fermeture card -->
         <div class="card" style="width: 18rem;">
             <div class="card-body">
-                <img src="img/pizza-02.png" class="card-img-top" alt="pizza2">
+                <img src="img/pizza-02.png" class="card-img-top flip" alt="pizza2">
                 <h5 class="card-title pub">Jalapeño Popper</h5>
                 <p class="price pub"><span class="dollar">$</span>11.80</p>
                 <div class="taille">
@@ -78,17 +80,17 @@
                 <select class="select pub" name="size" id="size"><option value="small ">Small</option><option value="medium">Medium</option></select>
                 </div> <!-- fermeture select-->
                 </div> <!-- fermeture taille-->
-                <div class="text">
                 <p class="card-text pub">All the flavors of a cheesy jalapeño popper is so much better in pizza form, with added bacon of course ! Imagine a jalapeño popper in the form of a pizza. Yes, it's as epic as it sounds. And better than a traidtional jalapeño popper in my opinion.</p>
-                <a href="#" class="btn btn-primary">Add to basket</a>
-                </div>
+                <a href="#" class="btn">Add to basket</a>
             </div> <!-- fermeture card-body -->
         </div> <!-- fermeture card -->
+        </div> <!-- fermeture cards -->
+        <h2 class="intro-title stf">who are we ?</h2>
     </div> <!-- fermeture menu -->
+    </div> <!-- fermeture menu0 -->
     <div class="about">
         <div class="introduction">
-            <h2 class="intro-title stf">who are we ?</h2>
-            <p class="pub">As in 1897, our goal is still to preserve the local Italian flavor and pass it to future generations !  For this, we constantly work and do research to obtain the most delicious and natural ingredients. With more than two hundred branches around the world, we are with you wherever you go !<br>You can reach us 24/7 from our website or mobile application, and you can enjoy this excellent taste at any time !</p>
+            <p class="pub">As in 1897, our goal is still to preserve the local Italian flavor and pass it to future generations !  For this, we constantly work and do research to obtain the most delicious and natural ingredients. With more than two hundred branches around the world, we are with you wherever you go !<br><br>You can reach us 24/7 from our website or mobile application, and you can enjoy this excellent taste at any time !</p>
         </div> <!-- fermeture introduction -->
         <div class="two_icons">
             <div class="ingredients">
